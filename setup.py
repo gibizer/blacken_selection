b307232db5fd666bd8c5f7b82a22ffed9451a2c7
Balazs Gibizer <gibizer@gmail.com>
2020-02-22 15:32:06 +0100

    Package for pypi

--- setup.py
+++ setup.py
@@ -6,11 +6,13 @@ with open("requirements.txt") as f:
 
 setup(
     name="blacken_selection",
-    version="0.1",
+    version="0.1.1",
     description="Apply black formatter for a piece of python source code",
     url="",
     author="Balazs Gibizer",
     author_email="gibizer@gmail.com",
+    home_page="https://github.com/gibizer/blacken_selection",
+    description_file="README.md",
     license="Apache License 2.0",
     packages=["blacken_selection"],
     zip_safe=False,
